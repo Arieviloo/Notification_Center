0d18830538ef0398a8e4dc35176a62fd12f4abb2
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-08-24 13:56:17 -0300

    btn

--- Notification_Center/Home/HomeView.swift
+++ Notification_Center/Home/HomeView.swift
@@ -22,8 +22,20 @@ class HomeView: UIView {
         $0.font = UIFont.systemFont(ofSize: 28)
         $0.tintColor = .gray
         return $0
-        
     }(UILabel())
+    
+    lazy var enterButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle("Clique aqui", for: .normal)
+        $0.tintColor = .black
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 8
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor(red: 207/255, green: 0/255, blue: 192/255, alpha: 1.0).cgColor
+        return $0
+        
+    }(UIButton(type: .system))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -38,6 +50,7 @@ class HomeView: UIView {
     private func configView() {
         addSubview(logoImageView)
         addSubview(titleLabel)
+        addSubview(enterButton)
     }
     
     private func configConstraints() {
@@ -46,7 +59,13 @@ class HomeView: UIView {
             logoImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -80),
             
             titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
-            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            enterButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            enterButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            enterButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            enterButton.heightAnchor.constraint(equalToConstant: 40)
+            
         ])
     }
     
